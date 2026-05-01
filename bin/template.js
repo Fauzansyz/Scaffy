abbce162eb50c23e1f1924f07723c285a07daf61
Fauzansyz <fauzanadzma2@gmail.com>
2026-05-01 08:24:48 +0700

    Structuring project

--- bin/template.js
+++ bin/template.js
@@ -1,68 +1,18 @@
 #!/usr/bin/env node
 
 const { Command } = require("commander");
-const fs = require("fs");
-const select = require("@inquirer/select").default;
+const createCommand = require('../src/commands/create')
 
 const path = require("path");
 
 const program = new Command();
 
 program
-	.name("zan")
-	.description("CLI generator")
-	.version("1.0.0");
+  .name("zan")
+  .description("CLI generator")
+  .version("1.0.0");
 
-program
-	.command("create <project-name>")
-	.option("-t, --template <template>", "template name", "react-js")
-	.action(async (projectName, options) => {
-		const answer = await select({
-			message: 'Select a package manager',
-			choices: [
-				{
-					name: 'npm',
-					value: 'npm',
-					description: 'npm is the most popular package manager',
-				},
-				{
-					name: 'yarn',
-					value: 'yarn',
-					description: 'yarn is an awesome package manager',
-				},
+program.addCommand(createCommand)
 
-				{
-					name: 'jspm',
-					value: 'jspm',
-					disabled: true,
-				},
-				{
-					name: 'pnpm',
-					value: 'pnpm',
-					disabled: '(pnpm is not available)',
-				},
-			],
-		});
-		const templateName = options.template;
-
-		const projectPath = path.join(process.cwd(), projectName);
-		const templatePath = path.join(__dirname, "../templates", templateName);
-
-		console.log("TEMPLATE:", templatePath);
-
-		if (!fs.existsSync(templatePath)) {
-			console.log("❌ Template tidak ditemukan");
-			return;
-		}
-
-		if (fs.existsSync(projectPath)) {
-			console.log("❌ Project sudah ada");
-			return;
-		}
-
-		fs.cpSync(templatePath, projectPath, { recursive: true });
-
-		console.log(`✅ Project ${projectName} berhasil dibuat`);
-	});
 
 program.parse();
---
+++ src/commands/create.js
@@ -0,0 +1,61 @@
+const { Command } = require("commander");
+const fs = require("fs");
+const select = require("@inquirer/select").default;
+
+const path = require("path");
+
+const create = new Command('create');
+
+create
+  .argument("<project-name>")
+  .option("-t, --template <template>", "template name", "react-js")
+  .action(async (projectName, options) => {
+    const answer = await select({
+      message: 'Select a package manager',
+      choices: [
+        {
+          name: 'npm',
+          value: 'npm',
+          description: 'npm is the most popular package manager',
+        },
+        {
+          name: 'yarn',
+          value: 'yarn',
+          description: 'yarn is an awesome package manager',
+        },
+
+        {
+          name: 'jspm',
+          value: 'jspm',
+          disabled: true,
+        },
+        {
+          name: 'pnpm',
+          value: 'pnpm',
+          disabled: '(pnpm is not available)',
+        },
+      ],
+    });
+    const templateName = options.template;
+
+    const projectPath = path.join(process.cwd(), projectName);
+    const templatePath = path.join(__dirname, "../templates", templateName);
+
+    console.log("TEMPLATE:", templatePath);
+
+    if (!fs.existsSync(templatePath)) {
+      console.log("❌ Template tidak ditemukan");
+      return;
+    }
+
+    if (fs.existsSync(projectPath)) {
+      console.log("❌ Project sudah ada");
+      return;
+    }
+
+    fs.cpSync(templatePath, projectPath, { recursive: true });
+
+    console.log(`✅ Project ${projectName} berhasil dibuat`);
+  });
+
+module.exports = create
